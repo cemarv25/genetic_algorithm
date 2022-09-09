e79fd6832a5381b3d5e139401a8487224ecd5bd8
cemarv25 <cesar.martinez0010@outlook.com>
2022-09-09 17:19:35 -0600

    finished algorithm

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,8 +1,16 @@
+from re import I
 from statistics import mean
 import random
 
 
-def evaluate_population(population: list[str]) -> list[int]:
+def log_generation_data(population: list[str], population_eval: list[int], generation_num: int):
+    print(f'--- Generation #{generation_num} Statistics ---')
+    print('\tBest individual: ', population[population_eval.index(max(population_eval))])
+    print('\tBest individual aptitude: ', max(population_eval))
+    print('\tAverage aptitude: ', mean(population_eval))
+    print('\n\n')
+
+def evaluate(population: list[str]) -> list[int]:
     max_aptitude = len(population[0])
     aptitudes = []
     for individual in population:
@@ -11,19 +19,57 @@ def evaluate_population(population: list[str]) -> list[int]:
     return aptitudes
 
 def select(population: list[str], population_eval: list[int]):
-    new_population = []
-    for _ in range(len(population)):
-        new_population.append()
+    total = sum(population_eval)
+    weights = [population_eval[i] / total for i in range(len(population))]
+    
+    return random.choices(population, weights, k=len(population))
+
+def reproduce(population: list[str]):
+    x = 0
+    y = 1
+    while y < len(population):
+        ind1 = population[x]
+        ind2 = population[y]
+
+        cross = random.randint(1, len(ind1) - 1)
+
+        offspring1 = ind1[:cross] + ind2[cross:]
+        offspring2 = ind2[:cross] + ind1[cross:]
+
+        population[x] = offspring1
+        population[y] = offspring2
+
+        x += 2
+        y += 2
+
+def mutate(population: list[str]):
+    while random.randint(0, 1):
+        individual_idx = random.randint(0, len(population) - 1)
+        gen_idx = random.randint(0, len(population[individual_idx]) - 1)
+
+        new_individual = list(population[individual_idx])
+        new_individual[gen_idx] = '0' if new_individual[gen_idx] == '1' else '1'
+        population[individual_idx] = ''.join(new_individual)
 
 def genetic(N = 14, L = 10):
+    generation_num = 1
     population = []
 
     for _ in range(N):
         population.append(f'{random.getrandbits(L):0{L}b}')
-    population_eval = evaluate_population(population)
+        
+    population_eval = evaluate(population)
+
+    log_generation_data(population, population_eval, generation_num)
 
     while mean(population_eval) < 1:
-        selection = select(population, population_eval)
+        population = select(population, population_eval)
+        reproduce(population)
+        mutate(population)
+        population_eval = evaluate(population)
+        
+        generation_num += 1
+        log_generation_data(population, population_eval, generation_num)
         
 
     return population
